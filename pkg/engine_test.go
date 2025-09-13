3a4c7df360a3cdc291edc62100c01bd79f667013
TheComputerM <mudit.somani00@gmail.com>
2025-09-13 07:51:42 +0000

    test: add test for existing engines

--- pkg/engine_test.go
+++ pkg/engine_test.go
@@ -1,6 +1,8 @@
 package pkg_test
 
 import (
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -19,4 +21,55 @@ func TestGetEngine(t *testing.T) {
 	engine, err := manager.Get("python")
 	require.NoError(t, err)
 	require.NotNil(t, engine)
+}
+
+func TestEngines(t *testing.T) {
+	testdataDir := "../test/engines"
+	files, err := os.ReadDir(testdataDir)
+	require.NoError(t, err)
+	require.Greater(t, len(files), 0)
+
+	// gets the files to mount for the given engine
+	getFiles := func(t *testing.T, engine string) []pkg.SandboxFile {
+		entries, err := os.ReadDir(filepath.Join(testdataDir, engine))
+		require.NoError(t, err)
+		files := make([]pkg.SandboxFile, 0, len(entries))
+		for _, entry := range entries {
+			if entry.IsDir() {
+				continue
+			}
+			name := entry.Name()
+			content, err := os.ReadFile(filepath.Join(testdataDir, engine, name))
+			require.NoError(t, err)
+			files = append(files, pkg.SandboxFile{
+				Name:    name,
+				Content: string(content),
+			})
+		}
+		return files
+	}
+
+	manager := getTestEngineManager()
+	engines, err := manager.List()
+	require.NoError(t, err)
+	require.Greater(t, len(engines), 0)
+	sandbox := pkg.Sandbox(0)
+
+	for _, engineName := range engines {
+		engine, err := manager.Get(engineName)
+		require.NoError(t, err)
+		t.Run(engineName, func(t *testing.T) {
+			require.NoError(t, sandbox.Init())
+			t.Cleanup(func() {
+				require.NoError(t, sandbox.Cleanup())
+			})
+			require.NoError(t, sandbox.Mount(getFiles(t, engineName)))
+			opts := defaultSandboxPhaseOptions()
+			opts.Stdin = "localbox"
+			execute, err := engine.Run(sandbox, opts)
+			require.NoError(t, err)
+			require.Equal(t, "OK", execute.Status)
+			require.Equal(t, "localbox", execute.Stdout)
+		})
+	}
 }
