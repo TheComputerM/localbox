1c4a086670d7afcef84f2364abaf27159cba393c
TheComputerM <mudit.somani00@gmail.com>
2025-09-13 08:55:15 +0000

    fix: go engine cache in sandbox

--- test/engines/go/main.go
+++ test/engines/go/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("localbox")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		fmt.Print(line)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+	}
 }
