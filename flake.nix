586c4da8aef5129e2153ec39ac7b1e92c69acbb7
TheComputerM <mudit.somani00@gmail.com>
2025-09-14 08:58:38 +0000

    chore: add flake.nix and flake.lock for Nix

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "LocalBox - general purpose sandbox for running untrusted code";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      rec {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          localbox = pkgs.buildGoModule {
+            name = "localbox";
+            src = self;
+            goSum = ./go.sum;
+            vendorHash = "sha256-5JK5tXVwioeH4yD1MCKI7UnxMjKdIwrNZ4kiDDDaVYQ=";
+            checkFlags = [ "-skip" ];
+          };
+          default = localbox;
+        };
+      }
+    );
+}
