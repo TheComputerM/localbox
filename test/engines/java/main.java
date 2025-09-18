963e80ea13570e25797d68bc1dbeeb29bbf13be9
TheComputerM <mudit.somani00@gmail.com>
2025-09-18 13:11:57 +0000

    feat: add java engine

---
+++ test/engines/java/main.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+
+public class main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+    }
+}
